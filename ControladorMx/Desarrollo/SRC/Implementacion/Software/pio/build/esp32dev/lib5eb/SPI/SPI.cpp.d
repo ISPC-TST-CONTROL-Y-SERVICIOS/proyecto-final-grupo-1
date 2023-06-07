b9c075f5c4df1fcfa2fb8eb40b4101b5710b0667
Leonardo Gonzalez <leogb37@gmail.com>
2023-06-07 10:04:38 -0300

    se ordena repo

---
+++ ControladorMx/Desarrollo/SRC/Implementacion/Software/pio/build/esp32dev/lib5eb/SPI/SPI.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/lib5eb/SPI/SPI.cpp.o: \
+ C:/Users/Usuario/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.cpp \
+ C:/Users/Usuario/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.h \
+ C:/Users/Usuario/.platformio/packages/framework-arduinoespressif32/variants/esp32/pins_arduino.h \
+ C:/Users/Usuario/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-spi.h
